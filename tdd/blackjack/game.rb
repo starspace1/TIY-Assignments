e58e44d8f287fdcc204b84c2f923fb23777d7780
Leslie Brown <lkb@alum.mit.edu>
2015-06-22 12:33:57 -0400

    Using new .blackjack? and .bust? methods in check_for_winner method

--- tdd/blackjack/game.rb
+++ tdd/blackjack/game.rb
@@ -12,9 +12,9 @@ class Game
   end
 
   def hit!
-    @player_hand.hit(@deck.draw)
+    success = @player_hand.hit(@deck.draw)
     print_hands
-    dealer_response(@dealer_hand)
+    success
   end
 
   def stay
@@ -47,21 +47,21 @@ class Game
     # If the value of the player's hand is 21, and the value of the dealer's hand is less, 
     # the game should display the message "Blackjack! Player wins." 
     # If both player and dealer have 21, the game should display the message "Push! No winner."
-    if (player_hand.value == 21)
+    if player_hand.blackjack?
       if (dealer_hand.value < 21)
         @winner = "Player"
         puts "Blackjack! Player wins." 
-      elsif (dealer_hand.value == 21)
+      elsif (dealer_hand.blackjack?)
         @winner = "Tie"
         puts "Push! No winner."
       end
     end
 
-    if (player_hand.value < 21)
-      if (dealer_hand.value > player_hand.value) && (dealer_hand.value <= 21)
+    if player_hand.value < 21
+      if (dealer_hand > player_hand) && (!dealer_hand.bust?)
         @winner = "Dealer"
         puts "Game over. Dealer wins."
-      elsif (dealer_hand.value < player_hand.value) && (dealer_hand.value <= 21)
+      elsif (dealer_hand < player_hand) && (!dealer_hand.bust?)
         @winner = "You"
         puts "Game over. You win."
       end
@@ -70,12 +70,12 @@ class Game
     # The player loses by ending up with a hand valued at greater than 21. 
     # Otherwise, the player wins by ending up with a hand value greater than the dealer's hand value, 
     # or if the dealer's hand value exceeds 21.
-    if (player_hand.value > 21)
+    if player_hand.bust?
       @winner = "Dealer"
       puts "You bust. Dealer wins."
     end
 
-    if (dealer_hand.value > 21)
+    if dealer_hand.bust?
       @winner = "You"
       puts "Dealer busts. You win!"
     end
