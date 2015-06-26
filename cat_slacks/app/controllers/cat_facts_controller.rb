14114315d7f2ffdc10a5b72b2db1a170c32de147
Leslie Brown <lkb@alum.mit.edu>
2015-06-26 16:22:42 -0400

    Initial cat_slacks app. no oauth yet.

---
+++ cat_slacks/app/controllers/cat_facts_controller.rb
@@ -0,0 +1,21 @@
+require 'JSON'
+require 'httparty'
+
+class CatFactsController < ApplicationController
+  def index
+    response = HTTParty.get('http://catfacts-api.appspot.com/api/facts?number=1')
+    cat_hash = JSON.parse(response)
+    @fact = cat_hash["facts"][0]
+  end
+
+  def send_message
+    response = HTTParty.post('https://slack.com/api/chat.postMessage', query: {
+      token: SLACK[:token],
+      channel: "C04U2G8SM",
+      text: params[:fact],
+      username: 'CatBot',
+      icon_url: 'http://lorempixel.com/g/100/100/cats/'
+    })
+   redirect_to root_url
+  end
+end
---
+++ cat_slacks/app/controllers/oauth_controller.rb
@@ -0,0 +1,3 @@
+class OauthController < ApplicationController
+  
+end
